80bff2bae058f8363a69ed333570c261d31d308c
featurive <bram.agjm1@gmail.com>
2014-12-21 19:34:19 -0800

    Minor change in BlockDutchFlag class.

--- src/main/java/com/featurive/bramcraft/block/BlockDutchFlag.java
+++ src/main/java/com/featurive/bramcraft/block/BlockDutchFlag.java
@@ -12,7 +12,7 @@ public class BlockDutchFlag extends ModBlock {
 
     @Override
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 1; i < 10; i++) {
             if (world.isAirBlock(x, y+i, z)) {
                 world.setBlock(x, y + i, z, BlockList.dutch_flag);
             }
